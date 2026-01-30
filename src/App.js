9de3baf0c5b93ea302f379ffbae644bac0a0647a
NABBEL SULTHAN <nabbelmech@gmail.com>
2026-01-30 23:11:19 +0530

    app.js fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
 import MainLayout from "./layout/MainLayout";
 import Chat from "./pages/Chat";
 import History from "./pages/History";
 
+const theme = createTheme();
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <MainLayout>
-        <Routes>
-          <Route path="/" element={<Chat />} />
-          <Route path="/history" element={<History />} />
-        </Routes>
-      </MainLayout>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <MainLayout>
+          <Routes>
+            <Route path="/" element={<Chat />} />
+            <Route path="/history" element={<History />} />
+          </Routes>
+        </MainLayout>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
